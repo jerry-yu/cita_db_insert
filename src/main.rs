9856ca20182da9112e08324d2efb3b894dfe86f9
yubo <yubo@cryptape.com>
2019-03-05 19:29:52 +0800

    fix bug

--- src/main.rs
+++ src/main.rs
@@ -2,53 +2,46 @@ extern crate bincode;
 extern crate cita_types;
 extern crate clap;
 extern crate common_types as types;
-extern crate db as cita_db;
+extern crate kvdb_rocksdb as cita_db;
 extern crate libproto;
 extern crate log;
 extern crate proof;
 extern crate rlp;
 
-use cita_db::{DBTransaction, Database, DatabaseConfig};
+use cita_db::{Database, DatabaseConfig};
 use cita_types::H256;
 use clap::App;
-/*use libproto::blockchain::{
-    AccountGasLimit as ProtoAccountGasLimit, Proof as ProtoProof, ProofType,
-};
-use rlp::{decode, encode, Decodable, Encodable};
-*/
-use std::path::Path;
-use types::db::{Readable, Writable};
-use types::header::*;
-use types::{db, extras, BlockNumber};
-
-use bincode::{serialize, Infinite};
-use proof::BftProof;
-use std::fs::{read_dir, remove_file, OpenOptions};
-use std::io::{self, Read, Seek, Write};
-use std::mem::transmute;
+//use types::db::{Readable, Writable};
+//use types::header::*;
 use std::time::Duration;
+use types::db;
 //use types::db::Key;
 
-const PTYPE: u8 = 5;
-const HTYPE: u8 = 4;
-const WAL: &str = "/wal";
-const NOSQL: &str = "/nosql";
-const STATEDB: &str = "/statedb";
+fn write_transaction(db: &Database, height: u64) {
+    let ch = H256::from("7cabfb7709b29c16d9e876e876c9988d03f9c3414e1d3ff77ec1de2d0ee59f66");
+    let cp = H256::from("7cabfb7709b29c16d9e876e876c9988d03f9c3414e1d3ff77ec1de2d0ee59f67");
 
+    let ch = ch.to_vec();;
+    let cp = cp.to_vec();
 
-fn write_transaction(height : u64) -> DBTransaction{
-    let hash = H256::new();
+    let mut batch = db.transaction();
+    let mut hash = H256::zero();
     hash.randomize();
-    let mut batch = DBTransaction::new();
-    batch.write(db::COL_EXTRA, &extras::CurrentHash, &hash);
+    batch.put_vec(db::COL_EXTRA, &ch.as_slice(), hash.to_vec());
 
-    let mut hdr = Header::default();
-    hdr.set_number(height);
-    batch.write(db::COL_HEADERS, &height, &hdr);
+    /*let mut hdr = Header::default();
+    hdr.set_number(height);*/
+    let tmp = (height % 256) as u8;
+    let tmps = vec![tmp; 1024];
+    batch.put_vec(db::COL_HEADERS, &height.to_be_bytes(), tmps);
 
-    let proof = hdr.proof();
-    batch.write(db::COL_EXTRA, &extras::CurrentProof, &proof);
-    batch
+    batch.put(db::COL_BODIES, &height.to_be_bytes(), &[0xc8]);
+
+    //let proof = hdr.proof();
+    //batch.put(db::COL_EXTRA, &cp.as_slice(), proof.get_content());
+    let proof = vec![1; 100];
+    batch.put_vec(db::COL_EXTRA, &cp.as_slice(), proof);
+    db.write(batch).unwrap();
 }
 
 fn main() {
@@ -61,7 +54,8 @@ fn main() {
         .args_from_usage("-d, --data=[PATH] 'Set data dir'")
         .get_matches();
 
-    let hi = matches.value_of("previos")
+    let hi = matches
+        .value_of("previos")
         .unwrap_or("0")
         .to_string()
         .parse::<u64>()
@@ -74,26 +68,24 @@ fn main() {
         .unwrap_or(3000);
     let data_path = matches.value_of("data").unwrap_or("./data");
 
-    let database_config = DatabaseConfig::with_columns(db::NUM_COLUMNS);
+    let mut database_config = DatabaseConfig::with_columns(db::NUM_COLUMNS);
+    database_config.memory_budget = Some(1024);
     let chain_db = Database::open(&database_config, &*data_path).expect("DB dir not right");
 
     for i in 0..hi {
-        let batch = write_transaction(i as u64);
-        chain_db.write(batch).unwrap();
+        if i % 10000 == 0 {
+            println!("speed mode,now height {:?}", i);
+        }
+        write_transaction(&chain_db, i as u64);
     }
 
     let mut hi = hi as u64;
     loop {
         hi += 1;
-        let batch = write_transaction(hi);
-        chain_db.write(batch).unwrap();
+        write_transaction(&chain_db, hi);
         ::std::thread::sleep(Duration::from_millis(delay));
+        if hi % 100 == 0 {
+            println!("relax mode,now height {:?}", hi);
+        }
     }
-
-
-
-
-
-
-
 }
